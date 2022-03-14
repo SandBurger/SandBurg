223b15ebf3bb061ffb8255c59059d5f0ae8dd372
dlxortmd987 <dlxortmd987@gmail.com>
2022-03-14 17:43:00 +0900

    Framework Setting #1

---
+++ src/main/java/com/sandburger/app/WorldBestServerApplication.java
@@ -0,0 +1,13 @@
+package com.sandburger.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldBestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldBestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandburger/app/WorldBestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandburger.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldBestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
