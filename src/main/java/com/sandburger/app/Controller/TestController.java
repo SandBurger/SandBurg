9e77df710afdc541486b86835aa34069e990216b
sangho-jeon <jeonsangho1587@gmail.com>
2022-05-17 18:12:18 +0900

    [publishing]

--- src/main/java/com/sandburger/app/Controller/TestController.java
+++ src/main/java/com/sandburger/app/Controller/TestController.java
@@ -2,6 +2,7 @@ package com.sandburger.app.Controller;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/{name}")
     @ApiOperation(value = "test", notes = "swagger test")
-    public String TestResponse(){
+    public String TestResponse(@PathVariable("name") String name){
 
+        return "slack test" + name;
+    }
+
+    @GetMapping(value = "/")
+    @ApiOperation(value = "test2", notes = "swagger test")
+    public String TestResponse2(){
         return "slack test";
     }
 }
